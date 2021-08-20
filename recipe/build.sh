298f1c4422f7c4fca98ac22c3c2530d8f9ddf9a6
Kai Tietz <ktietz@anaconda.com>
2021-08-20 16:40:53 +0200

    add osx-arm64 support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Get an updated config.sub and config.guess
+cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* .
+
 ./configure --prefix=$PREFIX --without-jni --host=$HOST
 
 export CFLAGS="-O2 -Wl,-S $CFLAGS"
